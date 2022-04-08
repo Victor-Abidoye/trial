62d5918d6aba8ac4b645cb9bf111b702e47b3559
Victor <abidoye8224@gmail.com>
2022-04-08 12:32:02 +0100

    Responsive Navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '101': '10vh',
+        '109': '90vh',
+      },
+      colors: {
+        prudent: {
+          100: "#2B3945",
+          200: "#202C37",
+        },
+        little: {
+          100: "#FFFFFF",
+          200: "#FAFAFA",
+        },
+      },
+    },
   },
   plugins: [],
 }
